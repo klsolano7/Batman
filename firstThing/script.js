2ba1e9edbe6dc86311f39533abbfe11ba83040bb
klsolano7 <klsolano77@gmail.com>
2018-08-14 20:13:19 -0400

    My first day went well

--- firstThing/script.js
+++ firstThing/script.js
@@ -2,3 +2,5 @@ var coolGuy =prompt("name a cool guy");
 console.log("the coolest person in the world is "+ coolGuy);
 coolGuy= "Charles"
 console.log("the coolest person in the world is "+ coolGuy);
+
+//Hi
