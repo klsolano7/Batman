6e9b01a89c3601346658bae7f3bfb87ba680a3ba
klsolano7 <ken.miami@yahoo.com>
2018-08-14 20:07:59 -0400

    I touched javascript file

---
+++ firstThing/script.js
@@ -0,0 +1,4 @@
+var coolGuy =prompt("name a cool guy");
+console.log("the coolest person in the world is "+ coolGuy);
+coolGuy= "Charles"
+console.log("the coolest person in the world is "+ coolGuy);
